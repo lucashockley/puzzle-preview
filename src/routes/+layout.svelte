2a60668bc0cf743857cfca33a466985b79a09131
Lucas Hockley <lucasgeorgehockley@gmail.com>
2025-07-20 18:43:44 +0100

    add og:image

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,10 @@
   let { children } = $props();
 </script>
 
+<svelte:head>
+  <meta property="og:image" content="https://puzzle-preview.tyma.app/preview.png" />
+</svelte:head>
+
 <div class="relative flex h-full flex-col p-5">
   {@render children()}
 </div>
